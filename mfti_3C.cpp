fe637f045de0d1df8e75d36a9d0f1cffae3b6cc6
MXLev <96011351+MXLev@users.noreply.github.com>
2022-08-17 18:49:32 +0600

    3C

--- mfti_3C.cpp
+++ mfti_3C.cpp
@@ -1,6 +1,6 @@
 //
 // Created by Lev Skuratov on 17.08.2022.
-// https://official.contest.yandex.ru/contest/39328/problems/A3/
+// https://official.contest.yandex.ru/contest/39328/problems/C3/
 
 #include <iostream>
 #include <deque>
@@ -14,7 +14,7 @@ typedef long long ll;
 int main(){
     int n, m;
     cin >> n >> m;
-    vector<vector<pair<ll, ll>>> g(m);
+    vector<vector<pair<ll, ll>>> g(n);
     vector<ll> who(n);
     for (int i = 0; i < n; ++i) {
         cin >> who[i];
@@ -25,16 +25,13 @@ int main(){
         --a, --b;
         if (who[b] != who[a]){
             g[a].push_back({b, 1});
-            g[b].push_back({a, 1});
         } else {
             g[a].push_back({b, 0});
-            g[b].push_back({a, 0});
         }
     }
 
     deque<ll> q;
     vector<ll> d(n, -1);
-    vector<ll> p(n);
     vector<ll> used(n);
 
     d[0] = 0;
@@ -44,7 +41,6 @@ int main(){
             if (!used[i]){
                 if (v == -1 || d[i] < d[v]){
                     v = i;
-                    p[i] = v;
                 }
             }
         }
@@ -59,7 +55,4 @@ int main(){
         }
     }
     cout << *min_element(d.begin(), d.end()) << ' ' << '\n';
-    for (ll i : p) {
-        cout << i << ' ';
-    }
 }
