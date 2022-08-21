73511b89bc1b23831829e39ec8b18f114db9fcd8
MXLev <96011351+MXLev@users.noreply.github.com>
2022-08-21 17:11:42 +0300

    5A

--- mfti_5A.cpp
+++ mfti_5A.cpp
@@ -6,34 +6,50 @@
 
 
 using namespace std;
-typedef long double ld;
+typedef long long int ll;
 
-vector<int> a;
+vector<ll> tree;
+vector<ll> prom;
 
-void update(){
-
+void push(ll v, ll lt, ll rt){
+    tree[v] += prom[v] * (rt - lt + 1);
+    prom[2 * v + 1] += prom[v];
+    prom[2 * v + 2] += prom[v];
+    prom[v] = 0;
 }
 
-void push(){
-
+void update(ll v, ll lt, ll rt, ll updateVAL, ll updateIND){
+    while (v != 0){
+        tree[v] = tree[v] - updateVAL;
+        v--;
+        v = v >> 1;
+    }
 }
 
-void build(){
+ll getSum(ll v, ll tl, ll tr, ll left, ll right){
+    push(v, tl, tr);
+    if (tl == left && tr == right){
+        return tree[v];
+    }
 
 }
 
 int main(){
-    int n, k;
+    ll n, k;
     cin >> n >> k;
-    a.resize(n);
+    tree.resize(4 * n);
+    fill(tree.begin(), tree.end(), 0);
+    prom.resize(4 * n);
+    fill(tree.begin(), tree.end(), 0);
+
     for (int i = 0; i < k; ++i) {
         char flag;
-        int index, value;
+        ll index, value;
         cin >> flag >> index >> value;
         if (flag == 'A'){
-
+            tree.push_back(value);
         } else if (flag == 'Q'){
-            cout << ;
+            cout << tree[i] << '\n';
         }
     }
 }
