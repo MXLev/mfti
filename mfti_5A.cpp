2632b68928a582b0aab791c0a1c8ca6484578d4e
MXLev <96011351+MXLev@users.noreply.github.com>
2022-08-21 14:20:21 +0300

    5A

---
+++ mfti_5A.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Lev Skuratov on 21.08.2022.
+// https://official.contest.yandex.ru/contest/39326/problems/A5/
+
+#include <bits/stdc++.h>
+
+
+using namespace std;
+typedef long double ld;
+
+vector<int> a;
+
+void update(){
+
+}
+
+void push(){
+
+}
+
+void build(){
+
+}
+
+int main(){
+    int n, k;
+    cin >> n >> k;
+    a.resize(n);
+    for (int i = 0; i < k; ++i) {
+        char flag;
+        int index, value;
+        cin >> flag >> index >> value;
+        if (flag == 'A'){
+
+        } else if (flag == 'Q'){
+            cout << ;
+        }
+    }
+}
